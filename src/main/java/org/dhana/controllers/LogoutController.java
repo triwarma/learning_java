e0997895de1b134884b06ee5510f46f67ad04973
DhanzNotFound <triwarmaardhana@gmail.com>
2026-06-03 12:38:57 +0700

    add feature logout

--- src/main/java/org/dhana/controllers/LogoutController.java
+++ src/main/java/org/dhana/controllers/LogoutController.java
@@ -1,6 +1,5 @@
 package org.dhana.controllers;
 
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,22 +9,15 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-
-@WebServlet("/dashboard")
-public class DashboardController extends HttpServlet {
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        if(session.getAttribute("has_login")== null){
-            resp.sendRedirect(req.getContextPath() + "/login");
-            return;
-
+        if(session !=null){
+            session.invalidate();
         }
-
-        req.getRequestDispatcher("WEB-INF/views/dashboard.jsp").forward(req,resp);
-
+        resp.sendRedirect(req.getContextPath()+"/login");
     }
-
-
 }
